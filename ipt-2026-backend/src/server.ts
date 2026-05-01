8c4ac85a8c4a568afcf3a286b0e65ac6ffcc5fc7
Kensu23 <kensumilagro@gmail.com>
2026-05-01 19:56:08 +0800

    Updated

---
+++ ipt-2026-backend/src/server.ts
@@ -0,0 +1,34 @@
+import express, { Application } from "express"
+import cors from "cors"
+import { errorHandler } from "./middleware/errorHandler"
+import { initialize } from "./_helpers/db"
+import userController from "./users/users.controller"
+import accountsController from "./accounts/accounts.controller"
+import cookieParser from "cookie-parser"
+
+const app: Application = express()
+
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(cors())
+app.use(cookieParser()) // <-------- the change 
+
+app.use("/accounts", accountsController)
+app.use("/users", userController)
+
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 4000
+
+
+initialize()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`SERVER IS RUNNING ON http://localhost:${PORT}`)
+            console.log(`TEST WITH: POST /users with {email, password, ....}`)
+        })
+    }).catch((err) => {
+        console.log(`Failed to initialize database::`, err)
+        process.exit(1)
+    })
